b5e0481f1a56b14eef2a9f96bd44d3f62e73dab2
ManishGru <gurumanish2015@gmail.com>
2022-08-12 00:43:50 +0545

    motor with mpu

--- self_balancing_robot/self_balancing_robot/self_balancing_robot.ino
+++ self_balancing_robot/self_balancing_robot/self_balancing_robot.ino
@@ -5,6 +5,8 @@
 
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
+#include "motor.h"
+
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
@@ -18,6 +20,12 @@ MPU6050 mpu;
 
 #define INTERRUPT_PIN 2 // use pin 2 on Arduino Uno & most boards
 #define LED_PIN 13		// (Arduino is 13, Teensy is 11, Teensy++ is 6)
+#define MOTOR1_PIN1 4
+#define MOTOR1_PIN2 7
+#define MOTOR1_PWMPIN 5
+#define MOTOR2_PIN1 8
+#define MOTOR2_PIN2 9
+#define MOTOR2_PWMPIN 6
 bool blinkState = false;
 
 // MPU control/status vars
@@ -33,6 +41,10 @@ Quaternion q;		 // [w, x, y, z]         quaternion container
 VectorFloat gravity; // [x, y, z]            gravity vector
 float ypr[3];		 // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
+//motor variables
+motor _motor;
+Motor_config motor_conf;
+
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
 // ================================================================
@@ -43,22 +55,22 @@ void dmpDataReady()
 	mpuInterrupt = true;
 }
 
+
+
+
+// ==================================================================
+// ===               			SETUP				              ===
+// ==================================================================
+
 void setup()
 {
 	// join I2C bus (I2Cdev library doesn't do this automatically)
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-	Wire.begin();
-	Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
-#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-	Fastwire::setup(400, true);
-#endif
+	I2Cinit();
 
 	// initialize serial communication
 	// (115200 chosen because it is required for Teapot Demo output, but it's
 	// really up to you depending on your project)
 	Serial.begin(115200);
-	while (!Serial)
-		; // wait for Leonardo enumeration, others continue immediately
 
 	// NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
 	// Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
@@ -67,6 +79,44 @@ void setup()
 	// crystal solution for the UART timer.
 
 	// initialize device
+	MPUinit();
+
+	// intialize motor
+	motorsetup();
+
+	// configure LED for output
+	pinMode(LED_PIN, OUTPUT);
+}
+
+
+// ==================================================================
+// ===               			LOOP				              ===
+// ==================================================================
+
+void loop()
+{
+	runmotor(&_motor);
+	if (!dmpReady)
+		return;
+	
+	readMPUdata();
+}
+
+
+
+
+
+
+void I2Cinit(){
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+	Wire.begin();
+	Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+	Fastwire::setup(400, true);
+#endif
+}
+
+void MPUinit(){
 	Serial.println(F("Initializing I2C devices..."));
 	mpu.initialize();
 	pinMode(INTERRUPT_PIN, INPUT);
@@ -75,24 +125,18 @@ void setup()
 	Serial.println(F("Testing device connections..."));
 	Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-	// wait for ready
-	Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-	while (Serial.available() && Serial.read())
-		; // empty buffer
-	while (!Serial.available())
-		; // wait for data
-	while (Serial.available() && Serial.read())
-		; // empty buffer again
 
 	// load and configure the DMP
 	Serial.println(F("Initializing DMP..."));
 	devStatus = mpu.dmpInitialize();
 
 	// supply your own gyro offsets here, scaled for min sensitivity
-	mpu.setXGyroOffset(220);
-	mpu.setYGyroOffset(76);
-	mpu.setZGyroOffset(-85);
-	mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+	mpu.setXGyroOffset(102);
+	mpu.setYGyroOffset(-14);
+	mpu.setZGyroOffset(-15);
+  	mpu.setXAccelOffset(-1871);
+  	mpu.setYAccelOffset(-527);
+	mpu.setZAccelOffset(507); // 1688 factory default for my test chip
 
 	// make sure it worked (returns 0 if so)
 	if (devStatus == 0)
@@ -130,19 +174,13 @@ void setup()
 		Serial.println(F(")"));
 	}
 
-	// configure LED for output
-	pinMode(LED_PIN, OUTPUT);
 }
 
-void loop()
-{
-	// if programming failed, don't try to do anything
-	if (!dmpReady)
-		return;
+void readMPUdata(){
 	// read a packet from FIFO
 	if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer))
 	{ // Get the Latest packet
-		// display Euler angles in degrees
+		// display gyro angles	 in degrees
 		mpu.dmpGetQuaternion(&q, fifoBuffer);
 		mpu.dmpGetGravity(&gravity, &q);
 		mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
@@ -156,4 +194,27 @@ void loop()
 		blinkState = !blinkState;
 		digitalWrite(LED_PIN, blinkState);
 	}
+}
+
+
+void motorsetup(){
+	 motor_config_init();
+    _motor = motor(&motor_conf);
+    _motor.motors_init();
+}
+
+void motor_config_init(){
+    motor_conf.motor1.outputpin1=4;
+    motor_conf.motor1.outputpin2=7;
+    motor_conf.motor1.speedpin=5;
+    motor_conf.motor1.speed=255;
+    motor_conf.motor1.direction=CLOCKWISE;
+	 
+ 
+
+    motor_conf.motor2.outputpin1=8;
+    motor_conf.motor2.outputpin2=9;
+    motor_conf.motor2.speedpin=6;
+    motor_conf.motor2.speed=255;
+    motor_conf.motor1.direction=CLOCKWISE;
 }
